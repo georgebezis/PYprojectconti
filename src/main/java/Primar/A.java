aaa8e8d864ed1b27b61e4b907e811446bba772f9
georgebezis <georgebezis31@gmail.com>
2024-03-24 13:46:48 +0200

    first commit

---
+++ src/main/java/Primar/A.java
@@ -0,0 +1,26 @@
+package Primar;
+
+
+public class A {
+    /**
+     *
+      * @param x face ce vrea el
+     * @return false
+     */
+   boolean gigel(int x)
+   {
+
+       return false;
+   }
+
+    /**
+     *
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+
+        System.out.println("gigel");
+
+    }
+}
---
+++ src/test/java/PR/Pip/ro/gigel.java
@@ -0,0 +1,8 @@
+package PR.Pip.ro;
+
+public class gigel {
+
+    public static void main(String[] args) {
+        // TODO: Auto-generated method stub
+    }
+}
